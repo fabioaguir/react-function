4cc48b510c300dfe94265b164ff713ea5e09933f
fabio aguiar <fabinhobarreto2@gmail.com>
2020-10-15 14:38:31 -0300

    Form finalizado, com prática de hooks, function components, pros e eventos de validação

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Container, Typography } from "@material-ui/core";
 
 export class App extends Component {
   render() {
     return (
-      <>
-        <h1>Formulário de cadastro</h1>
-        <FormularioCadastro />
-      </>
+      <Container maxWidth="sm" component="article">
+        <Typography variant="h3" component="h3" align="center">
+          Formulário de cadastro
+        </Typography>
+        <FormularioCadastro
+          aoEnviar={this.aoEnviarForm}
+          validarCpf={this.validarCpf}
+        />
+      </Container>
     );
+  }
+
+  aoEnviarForm(dados) {
+    console.log(dados);
+  }
+
+  validarCpf(cpf) {
+    if (cpf.length !== 11) {
+      return { valido: false, texto: "CPF deve ter 11 digitos" };
+    } else {
+      return { valido: true, texto: "" };
+    }
   }
 }
 
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { Button, FormControlLabel, Switch } from "@material-ui/core";
 
-function FormularioCadastro() {
-    return (
-        <form>
-            <input type='text'/>
-        </form>
-    )
+function FormularioCadastro({ aoEnviar, validarCpf }) {
+  const [nome, setNome] = useState("");
+  const [sobreNome, setSobreNome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [promocoes, setPromocoes] = useState(true);
+  const [novidades, setNovidades] = useState(true);
+  const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ nome, sobreNome, cpf, promocoes, novidades });
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          setNome(event.target.value);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        value={sobreNome}
+        onChange={(event) => {
+          setSobreNome(event.target.value);
+        }}
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        value={cpf}
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
+        onBlur={(evento) => {
+          const cpfValido = validarCpf(cpf);
+          setErros({ cpf: cpfValido });
+        }}
+        error={!erros.cpf.valido}
+        helperText={erros.cpf.texto}
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <FormControlLabel
+        label="Promoções"
+        control={
+          <Switch
+            checked={promocoes}
+            onChange={(event) => {
+              setPromocoes(event.target.checked);
+            }}
+            name="promocoes"
+            color="primary"
+          />
+        }
+      />
+      <FormControlLabel
+        label="Novidades"
+        control={
+          <Switch
+            checked={novidades}
+            onChange={(event) => {
+              setNovidades(event.target.checked);
+            }}
+            name="novidades"
+            color="primary"
+          />
+        }
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default FormularioCadastro;
